a75e3862a1f107447b81c25cea2aee0b70335a62
Aleksei Marov <alekseymmm@mail.ru>
2020-04-08 01:56:18 +0300

    add point rotate, crop triangle.

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
+
 #include <jpeglib.h>
 
 
@@ -136,10 +138,11 @@ int rotate_img(JSAMPARRAY src, int width, int height, float a, JSAMPARRAY dst)
 	for (y_new = 0; y_new < height; y_new++) {
 		for (x_new = 0; x_new < width; x_new++) {
 			x_dif = x_new - xc;
-			y_dif = y_new - yc;
+			//y_dif = y_new - yc;
+			y_dif = yc - y_new ;
 
-			x_old = xc + (x_dif * cosa - y_dif * sina);
-			y_old = yc + (x_dif * cosa + y_dif * sina);
+			x_old = xc + (x_dif * cosa + y_dif * sina);
+			y_old = yc - (- x_dif * sina + y_dif * cosa);
 			if (x_old < 0 || x_old >= width)
 				continue;
 			if (y_old < 0 || y_old >= height)
@@ -155,6 +158,145 @@ int rotate_img(JSAMPARRAY src, int width, int height, float a, JSAMPARRAY dst)
 	return 0;
 }
 
+struct point {
+	long x;
+	long y;
+};
+
+long sign (struct point *p1, struct point *p2, struct point *p3)
+{
+    return (p1->x - p3->x) * (p2->y - p3->y) - (p2->x - p3->x) * (p1->y - p3->y);
+}
+
+int point_in_triangle(struct point *pt, struct point *v1, struct point *v2,
+		      struct point *v3)
+{
+    float d1, d2, d3;
+    int has_neg, has_pos;
+
+    d1 = sign(pt, v1, v2);
+    d2 = sign(pt, v2, v3);
+    d3 = sign(pt, v3, v1);
+
+    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(has_neg && has_pos);
+}
+
+void crop_img_lower_triangle(JSAMPARRAY img, int width, int height)
+{
+	int x, y;
+	int x_dif;
+	struct point pt, v1;
+	struct point v2, v3;
+
+	v1.x = width / 2;
+	v1.y = height / 2;
+
+	x_dif = tan(M_PI / 6) * height / 2;
+
+	v2.x = v1.x - x_dif;
+	v2.y = height;
+
+	v3.x = v1.x + x_dif;
+	v3.y = height;
+
+	for (y = 0; y < height; y++) {
+		for (x = 0; x < width; x++) {
+			pt.x = x;
+			pt.y = y;
+			if (!point_in_triangle(&pt, &v1, &v2, &v3)) {
+				img[y][x * 3] = 0;
+				img[y][x * 3 + 1] = 0;
+				img[y][x * 3 + 2] = 0;
+			}
+		}
+	}
+
+}
+
+void rotate_point(struct point *pt, int width, int height, struct point *res,
+			bool counterclockwise)
+{
+	int x_new, y_new;
+	float x_dif, y_dif;
+	int xc = width / 2;
+	int yc = height / 2;
+
+	float a = M_PI / 3;
+	float cosa = cos(a);
+	float sina = sin(a);
+
+	x_dif = pt->x - xc;
+	//y_dif = pt->y - yc;
+	y_dif = yc - pt->y;
+	if (counterclockwise) {
+		x_new = xc + (x_dif * cosa - y_dif * sina);
+		y_new = yc - (x_dif * sina + y_dif * cosa);
+	} else {
+		x_new = xc + (x_dif * cosa + y_dif * sina);
+		y_new = yc - (-x_dif * sina + y_dif * cosa);
+	}
+	res->x = x_new;
+	res->y = y_new;
+}
+
+int fill_triangle(JSAMPARRAY img, int width, int height, struct point *v)
+{
+	int x, y;
+	struct point pt;
+
+	for (y = 0; y < height; y++) {
+		for (x = 0; x < width; x++) {
+			pt.x = x;
+			pt.y = y;
+			if (point_in_triangle(&pt, &v[0], &v[1], &v[2])) {
+				struct point rot;
+
+				rotate_point(&pt, width, height, &rot, false);
+
+				if (rot.x < 0 || rot.x > width - 1)
+					continue;
+				if (rot.y < 0 || rot.y > height -1)
+					continue;
+				img[y][x * 3] = img[rot.y][rot.x *3];
+				img[y][x * 3 + 1] = img[rot.y][rot.x *3 + 1];
+				img[y][x * 3 + 2] = img[rot.y][rot.x *3 + 2];
+			}
+		}
+	}
+}
+
+int fill_rotated_triangles(JSAMPARRAY img, int width, int height)
+{
+	int x, y;
+	int x_dif, y_dif;
+	struct point pt;
+	struct point v[3];
+	struct point new_v;
+
+	float cosa = cos(M_PI / 3);
+	float sina = sin(M_PI / 3);
+
+	v[0].x = width / 2;
+	v[0].y = height / 2;
+
+	x_dif = tan(M_PI / 6) * height / 2;
+
+	v[1].x = v[0].x - x_dif;
+	v[1].y = height;
+
+	v[2].x = v[0].x + x_dif;
+	v[2].y = height;
+// initial triangle filled
+	rotate_point(&v[2], width, height, &new_v, true);
+	v[1] = v[2];
+	v[2] = new_v;
+
+	fill_triangle(img, width, height, v);
+}
+
 int main(int argc, char **argv)
 {
 	int rc = 0;
@@ -165,8 +307,12 @@ int main(int argc, char **argv)
 	rc = libjpeg_read_file(INPUT_FILE_NAME, &img, &width, &height);
 
 	rot_img = alloc_img_buf(width * 3, height);
-	rotate_img(img, width, height, M_PI / 3, rot_img);
-	rc = libjpeg_write_file(OUTPUT_FILE_NAME, rot_img, width, height);
+
+	crop_img_lower_triangle(img, width, height);
+	fill_rotated_triangles(img, width, height);
+	//rotate_img(img, width, height, M_PI / 3, rot_img);
+
+	rc = libjpeg_write_file(OUTPUT_FILE_NAME, img, width, height);
 
 	free_img_buf(rot_img, width, height);
 	free_img_buf(img, width, height);
