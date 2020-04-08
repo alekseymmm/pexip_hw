024542a434e64903177f889aa0588f41728f04aa
Aleksei Marov <alekseymmm@mail.ru>
2020-04-08 23:29:17 +0300

    Scale initial image for triangles

--- main.c
+++ main.c
@@ -85,6 +85,56 @@ int libjpeg_read_file(char *file_name, JSAMPARRAY *img, int *width, int *height)
 }
 
 
+int libjpeg_read_file_scaled(char *file_name, JSAMPARRAY img, int *width, int *height)
+{
+	FILE *infile;
+	struct jpeg_decompress_struct decompress_info;
+	struct jpeg_decompress_struct *cinfo = &decompress_info;
+	struct jpeg_error_mgr jerr;
+	JSAMPARRAY buffer;
+	int row_stride;
+	int i;
+	int y_offset, x_offset;
+
+	infile = fopen(file_name, "rb");
+	if (!infile){
+		fprintf(stderr, "can not open file: %s\n", INPUT_FILE_NAME);
+		return -ENOENT;
+	}
+
+	cinfo->err = jpeg_std_error(&jerr);
+	jpeg_create_decompress(cinfo);
+
+	jpeg_stdio_src(cinfo, infile);
+	jpeg_read_header(cinfo, TRUE);
+
+	cinfo->scale_num = 1;
+	cinfo->scale_denom = 2;
+
+	jpeg_start_decompress(cinfo);
+	row_stride = cinfo->output_width * cinfo->output_components;
+
+
+	i = 0;
+	y_offset = cinfo->output_height - 1;
+	x_offset = cinfo->output_width / 2;
+	while (cinfo->output_scanline < cinfo->output_height) {
+		jpeg_read_scanlines(cinfo, &img[i], 1);
+		i++;
+	}
+
+	*width = cinfo->output_width;
+	*height = cinfo->output_height;
+
+
+	jpeg_finish_decompress(cinfo);
+	jpeg_destroy_decompress(cinfo);
+
+	fclose(infile);
+
+	return 0;
+}
+
 int libjpeg_write_file(char *file_name, JSAMPARRAY img, int width, int height)
 {
 	FILE *outfile;
@@ -475,24 +525,110 @@ int fill_rotated_triangles(JSAMPARRAY img, int width, int height)
 #endif
 }
 
+int img_scale_by_half(JSAMPARRAY src, int src_w, int src_h,
+		      JSAMPARRAY dst, int tgt_w, int tgt_h)
+{
+	int x, y;
+
+	for (y = 0; y < tgt_h; y++) {
+		int y2 = 2 * y;
+		for (x = 0; x < tgt_w; x++) {
+			int x2 = x * 2;
+			int i;
+			float p, q;
+
+			for (i = 0; i < 3; i++) {
+				p = src[y2][x2 * 3 + i] + src[y2][(x2+1)*3 + i];
+				p = p / 2;
+
+				q = src[y2+1][x2 * 3 + i] + src[y2+1][(x2+1)*3 + i];
+				q = q / 2;
+				dst[y][x*3 + i] = (p + q) / 2;
+			}
+		}
+	}
+}
+
+int fill_main_triangle(JSAMPARRAY img, int width, int height,
+		       JSAMPARRAY scaled_img, int scaled_width, int scaled_height)
+{
+	int x, y;
+	int x_dif;
+	struct point pt, v1;
+	struct point v2, v3;
+	int x_offset = width / 4;
+	int y_offset = height / 2;
+	v1.x = width / 2;
+	v1.y = height / 2 - 2;
+
+	x_dif = tan(M_PI / 6) * height / 2;
+
+	v2.x = v1.x - x_dif - 2;
+	v2.y = height;
+
+	v3.x = v1.x + x_dif + 2;
+	v3.y = height;
+
+	for (y = 0 ; y < height ; y++) {
+		for (x = 0; x < width; x++) {
+			pt.x = x;
+			pt.y = y;
+			if (!point_in_triangle(&pt, &v1, &v2, &v3) ||
+			    y - y_offset >= scaled_height ||
+			    y < y_offset ||
+			    x - x_offset >= scaled_width) {
+
+				float coef = 0.3;
+				img[y][x * 3] = img[y][x * 3] * coef;
+				img[y][x * 3 + 1] = img[y][x * 3 + 1] * coef;
+				img[y][x * 3 + 2] = img[y][x * 3 + 2] * coef;
+			} else {
+				img[y][x * 3] = scaled_img[y - y_offset][(x- x_offset)*3];
+				img[y][x * 3+ 1] = scaled_img[y - y_offset][(x- x_offset)*3 + 1];
+				img[y][x * 3+ 2] = scaled_img[y - y_offset][(x- x_offset)*3 + 2];
+			}
+		}
+	}
+
+}
+
+
 int main(int argc, char **argv)
 {
 	int rc = 0;
 	int width, height;
+	int scaled_width, scaled_height;
 	JSAMPARRAY img = NULL;
-	JSAMPARRAY rot_img = NULL;
+	JSAMPARRAY scaled_img = NULL;
+	char *input_filename;
+	char *output_file_name;
 
-	rc = libjpeg_read_file(INPUT_FILE_NAME, &img, &width, &height);
+	if (argc != 3) {
+		fprintf(stderr, "Wrong number of arguments. See usage:\n");
+		fprintf(stdout, "pexip_hw <input jpg file name> <output file name>\n");
+		exit(-EINVAL);
+	}
+	input_filename = argv[1];
+	output_file_name = argv[2];
 
-	rot_img = alloc_img_buf(width * 3, height);
+	rc = libjpeg_read_file(input_filename, &img, &width, &height);
+	scaled_width = width / 2;
+	scaled_height = height / 2;
+	scaled_img = alloc_img_buf(scaled_width * 3, scaled_height);
 
-	crop_img_lower_triangle(img, width, height);
+	img_scale_by_half(img, width, height, scaled_img, scaled_width, scaled_height);
+
+//	rc = libjpeg_read_file_scaled(INPUT_FILE_NAME, small_img, &small_width,
+//			 	      &small_height);
+	fill_main_triangle(img, width, height, scaled_img, scaled_width, scaled_height);
+	//crop_img_lower_triangle(img, width, height);
 	fill_rotated_triangles(img, width, height);
-	//rotate_img(img, width, height, -M_PI / 3, rot_img);
-	//rc = libjpeg_write_file(OUTPUT_FILE_NAME, rot_img, width, height);
-	rc = libjpeg_write_file(OUTPUT_FILE_NAME, img, width, height);
 
-	free_img_buf(rot_img, width, height);
+	rc = libjpeg_write_file(output_file_name, img, width, height);
+
+	free_img_buf(scaled_img, scaled_width, scaled_height);
 	free_img_buf(img, width, height);
+
+	printf("done. See result in %s\n", output_file_name);
 	return rc;
 }
